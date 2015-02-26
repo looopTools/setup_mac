2e5155645b5713375ff2b58a234a5a964ff4477f
Lars Nielsen <lnc13.lars@gmail.com>
2015-02-26 11:52:57 +0100

    update to setup

--- setup.sh
+++ setup.sh
@@ -12,3 +12,12 @@ brew install emacs --cocoa --srgb --with-gnutls
 brew install gcc
 brew install wget
 brew install aspell --with-all-langs #Dictonaries for Emacs
+brew install git
+brew install python3
+brew install markdown
+brew install tig
+
+
+# Download settings files
+# Check syntax for wget
+wget https://github.com/looopTools/setup_mac/blob/master/dot_emacs -O .emacs
